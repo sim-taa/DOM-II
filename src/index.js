64ffac6faffae98d0144167aa78eb35fce11d3e4
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-02-08 16:17:29 -0800

    Plays around with events. Four or five are working.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// //Button
+// const signMeUpFunSun = document.querySelector('.btn');
+
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Here you are.'
+
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+        .then(text => {
+            heading.textContent += text
+        })
+    })
+
+    document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = ''
+})
+
+document.body.addEventListener('select', evt => {
+    evt.target.textContent += 'boom'
+})
+}
